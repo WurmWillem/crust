4904671c9f6c0243a828d96f8034154f8b255f95
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2025-04-02 18:05:34 +0200

    well that was a lie lol

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
+use std::fmt::Display;
+
 pub type Byte = u8;
 
 #[repr(u8)]
 enum OpCode {
     Return,
+    Constant,
 }
 impl std::convert::From<u8> for OpCode {
     fn from(value: u8) -> Self {
         match value {
             0 => OpCode::Return,
+            1 => OpCode::Constant,
             _ => panic!("Not a valid opcode"),
         }
     }
 }
 
-fn simple_instruction(name: &str, offset: usize) -> usize {
-    println!("{}", name);
-    offset + 1
+#[derive(Debug)]
+enum Value {
+    F64(f64),
 }
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::F64(value) => write!(f, "{:?}", value),
+        }
+    }
+}
+// enum ValueType {
+//     Bool,
+//     Num,
+//     Nil,
+// }
 
 struct Chunk {
     code: Vec<Byte>,
+    constants: Vec<Value>,
 }
 impl Chunk {
     fn new() -> Self {
-        Self { code: vec![] }
+        Self {
+            code: vec![],
+            constants: vec![],
+        }
     }
 
     fn write_byte_to_chunk(&mut self, byte: Byte) {
         self.code.push(byte);
+    }
+
+    fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
     }
 
     fn disassemble(&mut self, name: &str) {
@@ -44,14 +69,32 @@ impl Chunk {
 
         let instruction = self.code[offset];
         match instruction.into() {
-            OpCode::Return => simple_instruction("OP_RETURN", offset),
+            OpCode::Return => Self::simple_instruction("OP_RETURN", offset),
+            OpCode::Constant => self.constant_instruction("OP_CONSTANT", offset),
             _ => panic!("Unreachable."),
         }
+    }
+
+    fn simple_instruction(name: &str, offset: usize) -> usize {
+        println!("{}", name);
+        offset + 1
+    }
+
+    fn constant_instruction(&self, name: &str, offset: usize) -> usize {
+        let constant_index = self.code[offset + 1];
+        print!("{} const index: {}", name, constant_index);
+        println!(" value: {}", self.constants[constant_index as usize]);
+        offset + 2
     }
 }
 
 fn main() {
     let mut chunk = Chunk::new();
+
+    let constant_index = chunk.add_constant(Value::F64(1.2));
+    chunk.write_byte_to_chunk(OpCode::Constant as u8);
+    chunk.write_byte_to_chunk(constant_index as u8);
+
     chunk.write_byte_to_chunk(OpCode::Return as u8);
     chunk.disassemble("test");
     // let x = vec![1, 2, 3];
