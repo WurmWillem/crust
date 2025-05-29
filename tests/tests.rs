087782f06a4a559c75338e96c3c8a7ba439616c2
Wurm Willem <wurmwillem@proton.me>
2025-05-29 22:24:51 +0200

    added some more tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -37,3 +37,41 @@ fn arr_reassignment() {
 
     assert_eq!(stdout.trim(), "123248");
 }
+
+#[test]
+fn continue_loop() {
+    let crust_file = "tests/continue.crs";
+
+    let output = Command::new("target/debug/crust")
+        .arg(crust_file)
+        .output()
+        .expect("Failed to run Crust interpreter");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    if !stderr.is_empty() {
+        eprintln!("Crust stderr:\n{}", stderr);
+    }
+
+    assert_eq!(stdout.trim(), "123458910");
+}
+
+#[test]
+fn break_loop() {
+    let crust_file = "tests/break.crs";
+
+    let output = Command::new("target/debug/crust")
+        .arg(crust_file)
+        .output()
+        .expect("Failed to run Crust interpreter");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    if !stderr.is_empty() {
+        eprintln!("Crust stderr:\n{}", stderr);
+    }
+
+    assert_eq!(stdout.trim(), "0123");
+}
